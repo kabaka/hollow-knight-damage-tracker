5768a226ba1f42dee7a25e7dc9a055f334d4eb95
Kyle Johnson <github.com@kyle.engineer>
2025-10-05 16:32:49 -0700

    refactor: relocate mobile stats bar component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,13 @@
 export { AppButton } from './AppButton';
 export { BossHealthBar } from './BossHealthBar';
+export { MobileStatsBar } from './MobileStatsBar';
 export { EncounterBrand } from './EncounterBrand';
 export { EncounterSummary } from './EncounterSummary';
 export { Modal } from './Modal';
 export { SurfaceSection } from './SurfaceSection';
 
 export type { BossHealthBarProps } from './BossHealthBar';
+export type { MobileStatsBarProps } from './MobileStatsBar';
 export type { EncounterBrandProps } from './EncounterBrand';
 export type { EncounterSummaryProps } from './EncounterSummary';
 export type { ModalProps } from './Modal';
